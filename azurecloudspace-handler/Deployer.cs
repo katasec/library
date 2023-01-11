a6e6282700f78388c726a1c4c1a865413a95275a
Ameer Deen <writeameer@gmail.com>
2023-01-11 20:39:20 +0400

    Testing minor update to handler

--- azurecloudspace-handler/Deployer.cs
+++ azurecloudspace-handler/Deployer.cs
@@ -2,18 +2,17 @@
 using System.Collections.Generic;
 using Pulumi;
 using ArkServer.Entities.Azure;
+using Resources = Pulumi.AzureNative.Resources;
 
 namespace AzureCloudspaceHandler;
 
 public static class Deployer
 {
-    public static Dictionary<string, object?> Start()
-    {
-        var exports = new Dictionary<string, object?>();
 
+    static AzureCloudspace GetConfig()
+    {
         var cfg = new Config();
         var configData = cfg.GetObject<AzureCloudspace>("arkdata");
-
         if (configData == null)
         {
             Console.WriteLine("Config data from Ark is empty, got null");
@@ -23,6 +22,18 @@ public static class Deployer
         {
             Console.WriteLine($"Hub name is: {configData.Hub.Name}");
         }
+
+        return configData;
+    }
+
+    public static Dictionary<string, object?> Start()
+    {
+        var exports = new Dictionary<string, object?>();
+
+        var cfg = GetConfig();
+
+        var resourceGroup = new Resources.ResourceGroup(cfg.Name);
+
 
         exports.Add("Hello", "World");
         exports.Add("Checking", "Update");
