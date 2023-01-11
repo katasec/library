ed4272c4e6770635b75b0da43e4e71176d051fb6
Ameer Deen <writeameer@gmail.com>
2023-01-11 10:58:18 +0400

    Updated KAtasec hello world to test arkdata input

--- katasec-go-helloworld/main.go
+++ katasec-go-helloworld/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	requests "github.com/katasec/ark/requests"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
 
 var (
@@ -10,7 +12,15 @@ var (
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		ctx.Export("Hello", "World")
+
+		cfg := config.New(ctx, "")
+
+		var data requests.AzureCloudspaceRequest
+		cfg.RequireObject("arkdata", &data)
+
+		ctx.Export("Hello", pulumi.String("World"))
+
+		ctx.Export("ArkData_Hub", pulumi.String(data.Hub.Name))
 		return nil
 	})
 }
