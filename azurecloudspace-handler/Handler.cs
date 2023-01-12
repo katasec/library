fdb9044bbb5d9cc92f1d4818f9e23c7343f07b5c
Ameer Deen <writeameer@gmail.com>
2023-01-12 11:46:08 +0400

    Adding networks

--- azurecloudspace-handler/Handler.cs
+++ azurecloudspace-handler/Handler.cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections.Generic;
 using Resources = Pulumi.AzureNative.Resources;
 using Network = Pulumi.AzureNative.Network;
+using System;
+using ArkServer.Entities.Azure;
+
 namespace AzureCloudspaceHandler;
 
 public static partial class Handler
 {
     public static string Location = "westus2";
+
+    public static void CreateAzureSpace()
+    {
+        var (hubRg, hubVnet, hubFw) = CreateHub();
+    }
+
     /// <summary>
     /// The Hub network is not directly visible to the end user. It is, however, implemented behind the scenes.
     /// </summary>
-    public static void CreateHub()
+    public static Tuple<Resources.ResourceGroup, Network.VirtualNetwork, Network.AzureFirewall> CreateHub()
     {
         // Create hub resource group
         var hubRg = new Resources.ResourceGroup("rg-hub-", new()
@@ -42,14 +51,38 @@ public static partial class Handler
             VirtualNetworkName = ConfigData.Hub.Name,
             Subnets = hubSubnets
         });
+
+        // Create Firewall
+        var firewall = CreateFirewall(hubRg, virtualNetwork);
+
+        return Tuple.Create(hubRg, virtualNetwork, firewall);
         //var subnet = new Network.Subnet()
     }
-
-
     /// <summary>
     /// Start creates the azure cloudspace which is essentially hub + one or more spokes
     /// </summary>
     /// <returns></returns>
+
+    public static void AddSpokes(Resources.ResourceGroup hubRg, Network.VirtualNetwork hubVnet, Network.AzureFirewall firewall)
+    {
+        foreach (var spoke in ConfigData.Env)
+        {
+            AddASpoke(spoke, hubRg, hubVnet, firewall);
+        }
+    }
+
+    public static void AddASpoke(VNetInfo spoke, Resources.ResourceGroup hubRg, Network.VirtualNetwork hubVnet, Network.AzureFirewall firewall)
+    {
+        // Create Spoke Resource Group
+        var spokeRgName = $"rg-{spoke.Name}";
+        var spokeRg = new Resources.ResourceGroup(spokeRgName);
+
+        // Route to the firewall
+        var routeName = $"rt-{spoke.Name}";
+        var spokeRoute = CreateFirewallRoute(spokeRg,firewall, routeName);
+
+    }
+
     public static Dictionary<string, object?> Start()
     {
         var exports = new Dictionary<string, object?>();
@@ -60,6 +93,5 @@ public static partial class Handler
         exports.Add("Checking", "Update");
         return exports;
     }
+
 }
-
-
---
+++ azurecloudspace-handler/Helpers.cs
@@ -0,0 +1,205 @@
+﻿using ArkServer.Entities.Azure;
+using System;
+using Pulumi;
+using Network = Pulumi.AzureNative.Network;
+using Resources = Pulumi.AzureNative.Resources;
+
+
+namespace AzureCloudspaceHandler;
+
+public static partial class Handler
+{
+    public static readonly AzureCloudspace ConfigData = new();
+
+    /// <summary>
+    /// The constructor reads pulumi config injected by the worker into this handler.
+    /// </summary>
+    static Handler()
+    {
+        Config cfg = new();
+        var azureCloudspace = cfg.GetObject<AzureCloudspace>("arkdata");
+        if (azureCloudspace == null)
+        {
+            Console.WriteLine("Config data from Ark is empty, got null");
+            Environment.Exit(0);
+        }
+        else
+        {
+            ConfigData = azureCloudspace;
+        }
+    }
+
+    public static Network.FirewallPolicy CreateFirewallPolicy(Resources.ResourceGroup rg) 
+    {
+        // Create a Firewall Policy
+        var firewallPolicy = new Network.FirewallPolicy("hub-policy-", new()
+        {
+            ResourceGroupName = rg.Name,
+            Sku = new Network.Inputs.FirewallPolicySkuArgs{
+                Tier = "Basic"
+            }
+        });
+
+        // Add Policy Rule to Firewall Policy
+        var firewallPolicyRuleCollectionGroup = new Network.FirewallPolicyRuleCollectionGroup("firewallPolicyRuleCollectionGroup", new()
+        {
+            Name = "apprules-",
+            Priority = 101,
+            FirewallPolicyName= firewallPolicy.Name,
+            RuleCollections = 
+            {
+                new Network.Inputs.FirewallPolicyFilterRuleCollectionArgs
+                {
+                    Name = "Allowed_HTTPs",
+                    Action = new Network.Inputs.FirewallPolicyFilterRuleCollectionActionArgs
+                    {
+                        Type = "Allow",
+                    },
+                    RuleCollectionType = "FirewallPolicyFilterRuleCollection",
+                    Rules = new[]
+                    {
+                        new Network.Inputs.ApplicationRuleArgs
+                        {
+                            Description = "Allow Github Container Registry",
+                            Name = "Allow_GHCR",
+                            Protocols = new[]
+                            {
+                                new Network.Inputs.FirewallPolicyRuleApplicationProtocolArgs
+                                {
+                                    Port = 443,
+                                    ProtocolType = "Https",
+                                },
+                            },
+                            RuleType = "ApplicationRule",
+                            SourceAddresses = new[]
+                            {
+                                "*"
+                            },
+                            DestinationAddresses= new[]
+                            {
+                                "ghcr.io"
+                            }
+                        },
+                    }
+                }
+
+            }
+        });
+
+
+        return firewallPolicy;
+    }
+
+    public static Network.AzureFirewall CreateFirewall(Resources.ResourceGroup rg,  Network.VirtualNetwork vnet) 
+    {
+        // Create a Firewall Policy for assigment later
+        var fwPolicy = CreateFirewallPolicy(rg);
+
+        // Create Firewall Management IP (used by Azure)
+        var managementIp = new Network.PublicIPAddress("fw-mgmt-ip", new()
+        {
+            ResourceGroupName= rg.Name,
+            PublicIPAllocationMethod = "Static",
+            Sku = new Network.Inputs.PublicIPAddressSkuArgs
+            {
+                Name = "Standard",
+                Tier = "Regional"
+            }
+        });
+
+        // Create public IP for firewall inbound/outbound traffic
+        var publicIp = new Network.PublicIPAddress("fw-ip", new()
+        {
+            ResourceGroupName= rg.Name,
+            PublicIPAllocationMethod = "Static",
+            Sku = new Network.Inputs.PublicIPAddressSkuArgs
+            {
+                Name = "Standard",
+                Tier = "Regional"
+            }
+        }); 
+
+        // Lookup firewall subnet Id
+        var fwSubnetId = Network.GetSubnet.Invoke(new Network.GetSubnetInvokeArgs{
+            ResourceGroupName= rg.Name,
+            SubnetName = "AzureFirewallSubnet",
+            VirtualNetworkName = ConfigData.Hub.Name
+        }).Apply((x) => {
+            return x.Id;
+         });
+        
+        // Lookup mgmt  subnet Id
+        var mgmtfwSubnetId = Network.GetSubnet.Invoke(new Network.GetSubnetInvokeArgs{
+            ResourceGroupName= rg.Name,
+            SubnetName = "AzureFirewallManagementSubnet",
+            VirtualNetworkName = ConfigData.Hub.Name
+        }).Apply((x) => {
+            return x.Id;
+         });
+
+        // Create Firewall
+        var firewall = new Network.AzureFirewall("hubfirewall",new()
+        {
+            ResourceGroupName= rg.Name,
+            Sku = new Network.Inputs.AzureFirewallSkuArgs
+            {
+                Tier= "Basic",
+                Name = "AZFW_VNet"
+            },
+            IpConfigurations = new Network.Inputs.AzureFirewallIPConfigurationArgs
+            {
+                Name = "fwip-configuration",
+                PublicIPAddress = new Network.Inputs.SubResourceArgs{
+                    Id = publicIp.Id,
+                },
+                Subnet = new Network.Inputs.SubResourceArgs
+                {
+                   Id = fwSubnetId
+                }  
+            },
+            ManagementIpConfiguration = new Network.Inputs.AzureFirewallIPConfigurationArgs
+            {
+                Name = "mgmtip-configuration",
+                PublicIPAddress = new Network.Inputs.SubResourceArgs{
+                    Id = managementIp.Id,
+                },
+                Subnet = new Network.Inputs.SubResourceArgs
+                {
+                   Id = mgmtfwSubnetId
+                }  
+            },
+            FirewallPolicy = new Network.Inputs.SubResourceArgs
+            {
+                Id = fwPolicy.Id
+            }
+        });
+
+        return firewall;
+    }
+
+    public static Network.RouteTable CreateFirewallRoute(Resources.ResourceGroup rg, Network.AzureFirewall firewall ,string rtTableName)
+    {
+        // Create Route Table
+        var routeTable = new Network.RouteTable(rtTableName, new()
+        {
+            ResourceGroupName= rg.Name,
+            RouteTableName= rtTableName
+        });
+
+        // Add a route to the firewall
+        var route = new Network.Route($"{rtTableName}-firewall-route", new()
+        {
+            AddressPrefix= "0.0.0.0/0",
+            NextHopType = "VirtualAppliance",
+            RouteTableName= rtTableName,
+            RouteName = "firewall-route",
+            NextHopIpAddress = firewall.IpConfigurations.First().Apply(x => x.PrivateIPAddress)
+        });
+        return routeTable;
+    }
+
+    public static void CreateVNet()
+    {
+
+    }
+}
