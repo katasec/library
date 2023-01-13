9c0e8ecaf6f46df949384c14ccd20579d0925e1e
Ameer Deen <writeameer@gmail.com>
2023-01-13 19:48:47 +0400

    added vnet dependency for firewall

--- azurecloudspace-handler/Helpers.cs
+++ azurecloudspace-handler/Helpers.cs
@@ -91,7 +91,7 @@ public static partial class Handler
         return firewallPolicy;
     }
 
-    public static Network.AzureFirewall CreateFirewall(Resources.ResourceGroup rg, CustomResourceOptions options = null) 
+    public static Network.AzureFirewall CreateFirewall(Resources.ResourceGroup rg, CustomResourceOptions? options = null) 
     {
         // Create a Firewall Policy for assigment later
         var fwPolicy = CreateFirewallPolicy(rg);
